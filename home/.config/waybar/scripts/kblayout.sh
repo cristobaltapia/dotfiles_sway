44d1306e248c509fb1c64cd8f7475c52f7279428
Cristóbal Tapia Camú <crtapia@gmail.com>
2020-10-18 19:52:54 +0200

    All with Nord colors

--- home/.config/waybar/scripts/kblayout.sh
+++ home/.config/waybar/scripts/kblayout.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-swaymsg -t get_inputs | grep -m1 "xkb_active_layout_name" | cut -d '"' -f4
+swaymsg -t get_inputs | jq -r \
+    "first(.[]|select(.identifier == \"$1\" and .type == \"keyboard\")) \
+    | .xkb_active_layout_name | .[0:3]"
+
+swaymsg -mrt subscribe '["input"]' | jq -r --unbuffered \
+    "select(.change == \"xkb_layout\")
+    | .input
+    | select(.identifier == \"$1\" and .type == \"keyboard\") \
+    | .xkb_active_layout_name | .[0:3]"
