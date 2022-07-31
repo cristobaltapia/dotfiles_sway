cdbcf3727082338885a3873e364b79479b9a1645
Cristóbal Tapia Camú <crtapia@gmail.com>
2022-07-31 23:47:15 +0200

    Update sway config

--- home/.config/sway/scripts/fuzzel-pass.sh
+++ home/.config/sway/scripts/fuzzel-pass.sh
@@ -3,8 +3,9 @@
 
 # Get all password files and create an array
 root=~/.password-store
-CACHE=~/.local/tmp/pass_wofi
 seat=seat0
+
+fuzzel_opt="--border-color=bf616aff"
 
 list_passwords() {
     shopt -s nullglob globstar
@@ -14,27 +15,24 @@ list_passwords() {
 
 }
 
-prompt='search for passwords...'
-SECRET=$(list_passwords | wofi -i --width 400 --lines 15 --prompt="${prompt}" --dmenu --cache-file ${CACHE} | \
+prompt='pass > '
+SECRET=$(list_passwords | fuzzel ${fuzzel_opt} -d --width=25 --lines=10 --prompt="${prompt}" | \
   awk '{print $2}')
 
 [[ -z "$SECRET" ]] && { echo "No secret chosen. Exiting..." ; exit 1; }
 
 # Ask whether pass, user or both are required
-
 options=("Password" \
         "User and password" \
         "User" \
         "QR-Code")
 
-option=$(printf '%s\n' "${options[@]%}" | wofi -i --dmenu --width 250 --lines 4 --prompt="..." --cache-file /dev/null)
-
-echo $option
+option=$(printf '%s\n' "${options[@]%}" | fuzzel ${fuzzel_opt} --dmenu --width 20 --lines 4 --prompt="> ")
 
 case ${option} in
   Password )
     # wlrctl keyboard type $(pass get_pass ${SECRET})
-    pass get_pass ${SECRET} | wl-copy
+    pass get_pass ${SECRET} | wl-copy -o
     ;;
   User )
     wlrctl keyboard type $(pass get_user ${SECRET})
@@ -43,7 +41,7 @@ case ${option} in
     wlrctl keyboard type $(pass get_user ${SECRET})
     ydotool key 15:1 15:0
     # wlrctl keyboard type $(pass get_pass ${SECRET})
-    pass get_pass ${SECRET} | wl-copy
+    pass get_pass ${SECRET} | wl-copy -o
     ;;
   "QR-Code" )
     if [[ $SECRET =~ wifi$ ]]; then
@@ -51,7 +49,7 @@ case ${option} in
       WIFISSID=$(pass get_user ${SECRET})
       WIFIPASS=$(pass get_pass ${SECRET})
       WIFIQR="WIFI:T:WPA;S:${WIFISSID};P:${WIFIPASS};;"
-      qrencode -s 8 -o - $WIFIQR | feh --title "pass: QR-WIFI" -
+      qrencode -s 8 -o - $WIFIQR | feh --title "pass: QR-WIFI" --info "echo 'WiFi: ${WIFISSID}'" -
     else
       # Only password
       pass show -q1 ${SECRET}
@@ -60,3 +58,4 @@ case ${option} in
 esac
 
 # wl-copy -o -s ${seat} ${PASSWD_PASS}
+
