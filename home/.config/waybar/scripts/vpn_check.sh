66f55bfffee3f57055a26677ab973b7e50e97a84
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-01-25 23:52:12 -0800

    waybar: update vpn detection

--- home/.config/waybar/scripts/vpn_check.sh
+++ home/.config/waybar/scripts/vpn_check.sh
@@ -3,6 +3,8 @@ if [ -d "/proc/sys/net/ipv4/conf/tun0" ]; then
     echo '{"text":"Connected","class":"connected","percentage":100}'
 elif [ -d "/proc/sys/net/ipv4/conf/ppp0" ]; then
     echo '{"text":"Connected","class":"connected","percentage":100}'
+elif [ -d "/proc/sys/net/ipv4/conf/peer_laptop" ]; then
+    echo '{"text":"Connected","class":"connected","percentage":50}'
 else
     echo '{"text":"Disconnected","class":"disconnected","percentage":0}'
 fi
